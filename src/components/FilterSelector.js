ce14e67a4c005aac714541df2c51b55e98f80066
Juan Miguel Sansinenea <juanmi.sansinenea@gmail.com>
2020-12-12 09:02:45 +0100

    filter selectors with selected values

--- src/components/FilterSelector.js
+++ src/components/FilterSelector.js
@@ -4,18 +4,31 @@ import "./FilterSelector.css";
 class FilterSelector extends Component {
   state = {
     expanded: false,
+    selected: "",
   };
   render() {
     return (
       <div>
         <div className="selector">
-          <button onClick={() => this.setState({ expanded: !this.state.expanded })}>
-            {this.props.text}
+          <button
+            onClick={() => this.setState({ expanded: !this.state.expanded })}
+          >
+            {this.props.text} {this.state.selected}
           </button>
         </div>
 
         {this.state.expanded === true
-          ? this.props.arr.map((el) => <div key={el}>{el}</div>)
+          ? this.props.arr.map((el) => (
+              <div
+                key={el}
+                onClick={() => {
+                  this.setState({ selected: el });
+                  this.setState({ expanded: false })
+                }}
+              >
+                {el}
+              </div>
+            ))
           : null}
       </div>
     );
