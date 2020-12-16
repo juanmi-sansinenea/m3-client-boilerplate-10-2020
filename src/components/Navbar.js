1a873cf0e21d0566d8181aa596cdadb6621d9197
Juan Miguel Sansinenea <juanmi.sansinenea@gmail.com>
2020-12-16 14:42:50 +0100

    mini fix in splash ahead of deploy

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,20 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { withAuth } from './../context/auth-context';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { withAuth } from "./../context/auth-context";
 
 class Navbar extends Component {
   render() {
     const { user, logout, isLoggedIn } = this.props;
-    user && console.log('user.username, isLoggedIn :>> ', user.username, isLoggedIn);
+    user &&
+      console.log("user.username, isLoggedIn :>> ", user.username, isLoggedIn);
     return (
+      user &&
       <nav className="navbar">
-        <Link to={'/classes'}>
+        <Link to={"/classes"}>
           <h4>Classes</h4>
         </Link>
-        {isLoggedIn ? (
-          <>
-            {/*<p>username: {user && user.username}</p>*/}
-            <h4 onClick={logout}>Logout</h4>
-          </>
-        ) : (
-          <>
-            <Link to="/login">
-              <button className="navbar-button">Login</button>{' '}
-            </Link>
-            <br />
-            <Link to="/signup">
-              <button className="navbar-button">Sign Up</button>{' '}
-            </Link>
-          </>
-        )}
+
+        <h4 onClick={logout}>Logout</h4>
       </nav>
     );
   }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { withAuth } from './../context/auth-context';
+import { Link } from 'react-router-dom';
 
 class Login extends Component {
   state = { username: "", password: "" };
@@ -33,6 +34,9 @@ class Login extends Component {
 
           <input type="submit" value="Login" />
         </form>
+        <Link to="/signup">
+              <p className="navbar-button">Sign Up</p>{' '}
+            </Link>
       </div>
     );
   }
